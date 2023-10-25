560e092d7c776435a8345386c725e7ba46e963f4
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2023-10-25 07:11:27 -0300

    commit25/10-01

---
+++ EXERCICIO 2/index.js
@@ -0,0 +1,15 @@
+/*Exercício 2
+Desenvolva uma função que receba um número como parâmetro e verifique se ele é par ou ímpar. Retorne true se for par e false se for ímpar.*/
+
+var numero = prompt("Digite um numero:")
+
+if (numero / 2 ) {
+    alert("Esse numero é par")
+}
+if(numero / 2 == 1 ) {
+    alert("Impar");
+}
+
+
+
+
