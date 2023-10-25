aadd190b5eb1dacfdb9fb4119dcc9b76aaa08a4c
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2023-10-25 08:59:01 -0300

    commit 25/10

--- EXERCICIO 2/index.js
+++ EXERCICIO 2/index.js
@@ -3,12 +3,13 @@ Desenvolva uma função que receba um número como parâmetro e verifique se ele
 
 var numero = prompt("Digite um numero:")
 
-if (numero / 2 ) {
+
+if (numero % 2 == 0 ) {
     alert("Esse numero é par")
+}else {
+    alert("Esse numero é impar")
 }
-if(numero / 2 == 1 ) {
-    alert("Impar");
-}
+
 
 
 
---
+++ EXERCICIO 3/index.js
@@ -0,0 +1,14 @@
+/*Exercício 3
+Implemente uma função que calcule a média aritmética de um array de números e retorne o resultado. Utilize essa função para calcular a média de diferentes conjuntos de números.*/
+
+
+var numeros = [02 , 05 , 19 , 50 , 03 ,24];
+var soma = 0;
+
+for (var i = 0; i < numeros.length; i++) {
+    var numero = numeros[i];
+    soma = soma + numeros;
+}
+
+var media = soma / numeros.length;
+alert(media);
